6ccd429a8ef0cf2c734832e37ffb65329f189237
Samantha Jayasinghe <isamantha@gmail.com>
2015-08-07 07:30:28 +0200

    setup project tests

---
+++ tests/FooTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class FooTest extends PHPUnit_Framework_TestCase{
+
+    protected function setUp()
+    {
+
+    }
+
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    protected function tearDown()
+    {
+
+    }
+} 
