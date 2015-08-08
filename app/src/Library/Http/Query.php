18e1c5b26cee3bd6fada9ca13c5ae38b01bbaf9e
Samantha Jayasinghe <isamantha@gmail.com>
2015-08-08 16:35:05 +0200

    Http Get interface for lib

--- app/src/Library/Http/Query.php
+++ app/src/Library/Http/Query.php
@@ -1,6 +1,6 @@
 <?php
 namespace Library\Http;
 
-class Query {
+abstract class Query {
 
 } 
---
+++ app/src/Library/Mapper/HttpGetMapper.php
@@ -0,0 +1,12 @@
+<?php
+namespace Library\Http\Mapper;
+
+ use Library\Http\Query;
+
+ interface HttpGetMapper {
+
+     public function getEndPoint(Query $query);
+
+     public function execute();
+
+} 
