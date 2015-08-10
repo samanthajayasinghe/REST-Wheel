e66c8818a0f89aaf2e2486d76777f2f5ffa921e8
Samantha Jayasinghe <isamantha@gmail.com>
2015-08-10 07:16:11 +0200

    add test for weather provider

---
+++ tests/Provider/OpenWeather/WeatherQueryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Provider\OpenWeather\Model\WeatherQuery;
+use Provider\OpenWeather\Mapper\WeatherMapper;
+
+class WeatherQueryTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var WeatherMapper
+     */
+    protected $mapper = null;
+
+    protected function setUp()
+    {
+        $this->mapper = new WeatherMapper();
+    }
+
+    public function testGetEndPointWithCityAndCountry()
+    {
+        $query = new WeatherQuery();
+        $query->setCity('Hamburg');
+        $query->setCountry('Germany');
+        $result = $this->mapper->getEndPoint($query);
+        $this->assertEquals('api.openweathermap.org/data/2.5/weather?q=Hamburg,Germany',$result);
+    }
+} 
